5baf8626a32f4218848a64eecf495b17c1398580
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-12-28 21:16:35 -0200

    subindo

--- hello-world-com-gpios-na-rpi3/led.py
+++ hello-world-com-gpios-na-rpi3/led.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO # Aqui e importado o pacote de GPIO para que assim o script teja acesso as GPIO da rpi
-import time # Importa o modulo de tempo - semelhante ao delay do arduino
+import time # Importa o pacto time - semelhante ao delay do arduino
 
 GPIO.setwarnings(False) # Aqui e setado os 'warnings' como falso, para nao mostra-los no console
 GPIO.setmode(GPIO.BCM) # Seta o tipo de pinagem da gpio, neste caso sera o BCM, que sao os alieases aos pinos nativos
@@ -13,10 +13,8 @@ def led( pin, delay ):
     time.sleep(delay)
 
 try:
-
 # Aqui e o loop do script - semelhante ao loop() do arduino
     while True:
-
         led(17, 1) # invoca a funcao led passando como parametro o pino do led bem como o tempo de delay, que neste caso e 1 segundo
 
 except KeyboardInterrupt:
