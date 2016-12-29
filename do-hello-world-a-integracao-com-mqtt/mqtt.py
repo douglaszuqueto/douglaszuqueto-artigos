a15d558dbea11660efa6d8ed49f69f45ebfea7f5
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-12-28 23:48:24 -0200

    mqtt

--- do-hello-world-a-integracao-com-mqtt/mqtt.py
+++ do-hello-world-a-integracao-com-mqtt/mqtt.py
@@ -1,28 +1,39 @@
 import sys
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt # importa o pacote mqtt
 import RPi.GPIO as GPIO
 
 ledPin = 17
-broker = "test.mosquitto.org"
-port = 1883
-keppAlive = 60
-topic = 'DZ/#'
+
+broker = "test.mosquitto.org" # define o host do broker mqtt'
+port = 1883 # define a porta do broker
+keppAlive = 60 # define o keepAlive da conexao
+topic = 'DZ/#' # define o topico que este script assinara
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ledPin, GPIO.OUT)
 
+# funcao on_connect será atribuida e chamada quando a conexao for iniciada
+# ela printara na tela caso tudo ocorra certo durante a tentativa de conexao
+# tambem ira assina o topico que foi declarado acima
 def on_connect(client, userdata, flags, rc):
     print("[STATUS] Conectado ao Broker. Resultado de conexao: "+str(rc))
 
     client.subscribe(topic)
 
+# possui o mesmo cenario que o on_connect, porem, ela sera atrelada ao loop
+# do script, pois toda vez que receber uma nova mensagem do topico assinado, ela sera invocada
 def on_message(client, userdata, msg):
-    message = str(msg.payload)
-    print("[MSG RECEBIDA] Topico: "+msg.topic+" / Mensagem: "+message)
 
+    messagem = str(msg.payload) # converte a mensagem recebida
+    print("[MSG RECEBIDA] Topico: "+msg.topic+" / Mensagem: "+messagem) # imprime no console a mensagem
+
+    # testara se o topico desta mensagem sera igual ao topico que queremos, que neste caso remete ao led
     if msg.topic == 'DZ/rasp/led':
-        if(message == '1'):
+
+        # basicamente nessa condicional testara se o valor recebido sera 1, sendo 1 acende o led
+        # e, caso receber qualquer outra coisa, apagara o led
+        if(messagem == '1'):
             GPIO.output(ledPin, GPIO.HIGH)
         else:
             GPIO.output(ledPin, GPIO.LOW)
@@ -30,12 +41,13 @@ def on_message(client, userdata, msg):
 try:
     print("[STATUS] Inicializando MQTT...")
 
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
+    client = mqtt.Client() # instancia a conexao
+    client.on_connect = on_connect # define o callback do evento on_connect
+    client.on_message = on_message # define o callback do evento on_message
 
-    client.connect(broker, port, keppAlive)
-    client.loop_forever()
+    client.connect(broker, port, keppAlive) # inicia a conexao
+    client.loop_forever() # a conexao mqtt entrara em loop ou seja, ficara escutando e processando todas mensagens recebidas
+
 except KeyboardInterrupt:
-    print "\nCtrl+C pressionado, encerrando aplicacao e saindo..."
+    print "\nScript finalizado."
     sys.exit(0)
