1be14620cc523276ce48c1f581dabcd420f69717
douglaszuqueto <douglas.zuqueto@gmail.com>
2017-01-05 18:51:17 -0200

    dev

--- criando-uma-pagina-web-para-controlar-o-led/static/script.js
+++ criando-uma-pagina-web-para-controlar-o-led/static/script.js
@@ -1,14 +1,14 @@
-
+var topic = 'DZ/rasp/led';
 var mqtt = new Paho.MQTT.Client(
-  "test.mosquitto.org",
-  8080,
-  "DZ"
+    "test.mosquitto.org",
+    8080,
+    "DZ"
 );
 
 mqtt.onConnectionLost = onConnectionLost;
 mqtt.onMessageArrived = onMessageArrived;
 
-function onConnectionLost(responseObject){
+function onConnectionLost(responseObject) {
     return console.log("Status: " + responseObject.errorMessage);
 }
 function onMessageArrived(message) {
@@ -17,34 +17,48 @@ function onMessageArrived(message) {
 
 // MQTT
 var options = {
-  timeout: 3,
-  onSuccess: onSuccess,
-  onFailure: onFailure
+    timeout: 3,
+    onSuccess: onSuccess,
+    onFailure: onFailure
 };
 function onSuccess() {
-  console.log("Conectado com o Broker MQTT");
-  mqtt.subscribe('DZ/rasp/led', {qos: 1}); // Assina o Tópico led/1
-  $(".container a").removeClass('disabled');
-};
+    console.log("Conectado com o Broker MQTT");
+    mqtt.subscribe(topic, {qos: 1}); // Assina o Tópico led/1
+    $(".container a").removeClass('disabled');
+}
 
 function onFailure(message) {
-  console.log("Connection failed: " + message.errorMessage);
-};
+    console.log("Connection failed: " + message.errorMessage);
+}
 
 // APP
 function init() {
-  mqtt.connect(options); // Conecta ao Broker MQTT
-};
-function led(value){ // Evento do Botão Desligar
+    mqtt.connect(options); // Conecta ao Broker MQTT
+}
+function led(value) { // Evento do Botão Desligar
     message = new Paho.MQTT.Message(value); // Cria uma nova mensagem
-    message.destinationName = "DZ/rasp/led"; // Define o tópico a ser enviado, neste caso: led/1
+    message.destinationName = topic; // Define o tópico a ser enviado, neste caso: led/1
     mqtt.send(message); // Envia a mensagem
-};
+}
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $('#btn-on').click(function(){ led("1"); });
-  $('#btn-off').click(function(){ led("0"); });
+    $('#save').on('click', function () {
+        var broker, topic;
+
+        broker = $('#broker').val();
+        topic = $('#topic').val();
+
+        console.log(broker, topic);
+
+        return false;
+    });
+    $('#btn-on').click(function () {
+        led("1");
+    });
+    $('#btn-off').click(function () {
+        led("0");
+    });
 
 });
 init();
