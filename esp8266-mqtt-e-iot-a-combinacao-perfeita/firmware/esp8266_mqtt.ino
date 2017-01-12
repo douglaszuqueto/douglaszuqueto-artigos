548c1a9b8bb3a5b113d65a94798a9f27aaec9e19
douglaszuqueto <douglas.zuqueto@gmail.com>
2017-01-12 08:12:24 -0200

    add esp8266-mqtt

---
+++ esp8266-mqtt-e-iot-a-combinacao-perfeita/firmware/esp8266_mqtt.ino
@@ -0,0 +1,103 @@
+// Libs
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Vars
+const char* SSID = "homewifi_D68"; // rede wifi
+const char* PASSWORD = "09114147"; // senha da rede
+
+const char* BROKER_MQTT = "broker.iot-br.com"; // ip/host do broker
+int BROKER_PORT = 8080; // porta do broker
+
+// prototypes
+void initPins();
+void initSerial();
+void initWiFi();
+void initMQTT();
+
+WiFiClient espClient;
+PubSubClient MQTT(espClient); // instancia o mqtt
+
+// setup
+void setup() {
+  initPins();
+  initSerial();
+  initWiFi();
+  initMQTT();
+}
+
+void loop() {
+  if (!MQTT.connected()) {
+    reconnectMQTT();
+  }
+  recconectWiFi();
+  MQTT.loop();
+}
+
+// implementacao dos prototypes
+
+void initPins() {
+  pinMode(D5, OUTPUT); // Led Ligado ao Pino D5 do ESP826 - ESP12-E
+  digitalWrite(D5, 0);
+}
+
+void initSerial() {
+  Serial.begin(115200);
+}
+void initWiFi() {
+  delay(10);
+  Serial.println("Conectando-se em: " + String(SSID));
+  
+  WiFi.begin(SSID, PASSWORD); // Conecta na Rede Wireless
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("Conectado na Rede " + String(SSID) + " | " + String(WiFi.localIP()));
+}
+
+// Funcão para se conectar ao Broker MQTT
+void initMQTT() {
+  MQTT.setServer(BROKER_MQTT, BROKER_PORT);
+  MQTT.setCallback(mqtt_callback);
+}
+
+//Função que recebe as mensagens publicadas
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+
+  String message;
+  for (int i = 0; i < length; i++) {
+    char c = (char)payload[i];
+    message += c;
+  }
+  Serial.println("Tópico => " + String(topic) + " | Valor => " + String(message));
+  if (message == "1") {
+    digitalWrite(D5, 1);
+  } else {
+    digitalWrite(D5, 0);
+  }
+  Serial.flush();
+}
+
+void reconnectMQTT() {
+  while (!MQTT.connected()) {
+    Serial.println("Tentando se conectar ao Broker MQTT: " + String(BROKER_MQTT));
+    if (MQTT.connect("ESP8266-ESP12-E")) {
+      Serial.println("Conectado");
+      MQTT.subscribe("DZ/esp8266-01/led");
+
+    } else {
+      Serial.println("Falha ao Reconectar");
+      Serial.println("Tentando se reconectar em 2 segundos");
+      delay(2000);
+    }
+  }
+}
+
+void recconectWiFi() {
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+}
