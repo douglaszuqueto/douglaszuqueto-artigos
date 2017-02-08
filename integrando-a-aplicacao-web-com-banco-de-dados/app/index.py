9b76b34ce75f4e5269ee8615320df17f8be28b76
douglaszuqueto <douglas.zuqueto@gmail.com>
2017-02-08 19:32:02 -0200

    atualização do index.py

--- integrando-a-aplicacao-web-com-banco-de-dados/app/index.py
+++ integrando-a-aplicacao-web-com-banco-de-dados/app/index.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 debug = True
 app   = Flask(__name__)
 
+# funcao responsavel por efetuar a leitura do banco de dados e nos retornar as temperaturas cadastradas
 def getTemperature():
     conn   = sqlite3.connect('database.db')
     cursor = conn.cursor()
@@ -20,6 +21,9 @@ def getTemperature():
 
 @app.route('/')
 def index():
+    # veja que abaixo passamos um atributo 'temperatures' no metodo render_template
+    # isso nada mais e que estamos passando as temperaturas recuperadas para nosso html
+    # para que depois se possa mostrar os dados na tabela
     return render_template('index.html', temperatures=getTemperature())
 
 if __name__ == "__main__":
