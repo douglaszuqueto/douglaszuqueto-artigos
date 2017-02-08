c816641d7d4d4aa13c02e5021254bcbfb899bbb7
douglaszuqueto <douglas.zuqueto@gmail.com>
2017-02-08 19:22:42 -0200

    atualização do index.py

--- integrando-a-aplicacao-web-com-banco-de-dados/app/index.py
+++ integrando-a-aplicacao-web-com-banco-de-dados/app/index.py
@@ -1,13 +1,11 @@
-# importa os pacotes
 import sqlite3
 from flask import Flask, render_template
 
 debug = True
-# instancia o flask
-app = Flask(__name__)
+app   = Flask(__name__)
 
 def getTemperature():
-    conn = sqlite3.connect('database.db')
+    conn   = sqlite3.connect('database.db')
     cursor = conn.cursor()
 
     cursor.execute("""
@@ -20,18 +18,11 @@ def getTemperature():
 
     conn.close()
 
-# define uma rota, neste caso a rota raiz
 @app.route('/')
 def index():
-    # render_template como o nome ja diz, ira renderizar nosso index.html
     return render_template('index.html', temperatures=getTemperature())
 
 if __name__ == "__main__":
-    # inicia a aplicacao
-    # 2 parametros sao passados, host e port
-    # host = seu ip, neste caso sera 0.0.0.0 pois rodara em todas interfaces de rede
-    # port = porta que sera usada, neste caso iremos rodar na porta 80
-
     if debug:
         app.run(host='0.0.0.0', port=80, debug=True)
     else:
