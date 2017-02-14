f2ef36b7761050b3cef8e979a61d9cedafa6b84c
douglaszuqueto <douglas.zuqueto@gmail.com>
2017-02-14 18:36:11 -0200

    Update: acionamento de reles

---
+++ shield-de-automacao-interface-de-controle-para-acionamento-dos-reles/raspberry/mqtt.py
@@ -0,0 +1,50 @@
+import sys
+import time
+import paho.mqtt.client as mqtt
+
+import Adafruit_GPIO.MCP230xx as MCP
+import Adafruit_GPIO as GPIO
+
+broker = "broker.iot-br.com"
+port = 8080
+keppAlive = 60
+topic = 'DZ/#'
+
+mcp = MCP.MCP23017()
+mcp.setup(0, GPIO.OUT)
+
+
+def on_connect(client, userdata, flags, rc):
+  print("[STATUS] Conectado ao Broker. Resultado de conexao: " + str(rc))
+
+  client.subscribe(topic)
+
+
+def on_message(client, userdata, msg):
+  message = str(msg.payload)  # converte a mensagem recebida
+  print("[MSG RECEBIDA] Topico: " + msg.topic + " / Mensagem: " + message)
+
+  topic = str(msg.topic).split('/')
+  releName = int(topic[2])
+
+  if message == '1':
+    mcp.output(releName, True)
+  else:
+    mcp.output(releName, False)
+
+  time.sleep(0.1)
+
+
+try:
+  print("[STATUS] Inicializando MQTT...")
+
+  client = mqtt.Client()
+  client.on_connect = on_connect
+  client.on_message = on_message
+
+  client.connect(broker, port, keppAlive)
+  client.loop_forever()
+
+except KeyboardInterrupt:
+  print "\nScript finalizado."
+  sys.exit(0)
